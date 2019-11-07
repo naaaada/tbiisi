97f88f27973de32d6328b1672df6d8b0d7cbb6dc
Nada <naada0505@gmail.com>
2019-11-07 17:54:16 +0700

    second commit

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "root";
+	$host = "ec2-54-235-86-101.compute-1.amazonaws.com";
+	$user = "exfbqmxjugzkns";
+	$pass = "debbfe78cd4cf3d712d026844af20511794b4a587d307c526e5112ffed0266ef";
 	$port = "5432";
-	$dbname = "hotel";
+	$dbname = "d759llilpdk0ec";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
